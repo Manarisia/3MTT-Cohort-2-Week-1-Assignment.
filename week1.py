bbb31e28fb8566d96aa350a2b8ea8eb720f68b1f
Manarisia <135823220+Manarisia@users.noreply.github.com>
2024-08-27 09:17:39 +0100

    Add files via upload

---
+++ week1.py
@@ -0,0 +1,29 @@
+#Assignment no:1. Write a Python script to calculate basic statistics (e.g., sum, average, min, max).
+a = 10
+b = 8
+c = 7
+d = 5
+
+#ADDITION
+addition = a+b+c+d
+print(addition)
+
+#AVERAGE
+import statistics
+print(statistics.mean({10,8,7,5}))
+
+#MAX
+x = max(10,8,7,5)
+print(x)
+
+#MIN
+y = min(10,8,7,5)
+print(y)
+
+
+# Assignment no: 2.Convert an integer to a float
+# An integer is a whole number, it can be 0, negative or positive number. While float is a number with decimal points.
+x = 2 #int
+a = float(x)
+
+print(a)
